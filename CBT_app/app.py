d580e7655bc3c9c0df054b372454fada021db13b
MAAUN/24/SWE/0008 <hauwaumuhammad018@gmail.com>
2026-03-20 02:19:34 +0100

    Added questions list (data structure)

--- CBT_app/app.py
+++ CBT_app/app.py
@@ -5,3 +5,9 @@ class Question:
 
     def check_answer(self, user_answer):
         return user_answer.lower() == self.answer.lower()
+
+questions = [
+    Question("What is the pinnacle of motorsport?", "Formula1"),
+    Question("Who was the 2020 Formula1 World Champion?", "Lewis Hamilton"),
+    Question("What language is used in Flask?", "Python")
+]
